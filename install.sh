d1e09b998641d42f3e1e8e08db1f9b0575a672a1
Saltyfunnel <gerardsheal@gmail.com>
2025-08-13 20:03:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,134 +1,92 @@
 #!/bin/bash
 
-# --- Variables ---
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-USER_NAME="${SUDO_USER:-$USER}"
-USER_HOME=$(eval echo "~$USER_NAME")
-CONFIG_DIR="$USER_HOME/.config"
-REPO_DIR="$USER_HOME/hyprdracula"
-ASSETS_SRC="$REPO_DIR/assets"
-ASSETS_DEST="$CONFIG_DIR/assets"
+USER_HOME="/home/$SUDO_USER"
 
-# --- Helper functions ---
-log() { echo -e "\e[32m[INFO]\e[0m $1"; }
-warn() { echo -e "\e[33m[WARN]\e[0m $1"; }
-error() { echo -e "\e[31m[ERROR]\e[0m $1"; }
-run() { 
-    log "$2"
-    eval "$1"
-    if [ $? -ne 0 ]; then error "$2 failed"; exit 1; fi
-}
+# Helper functions
+print_header() { echo -e "\n===== $1 =====\n"; }
+print_info() { echo -e "[INFO] $1"; }
+print_success() { echo -e "[OK] $1"; }
+run_command() { echo -e "[RUN] $1"; eval "$1"; }
 
-copy_as_user() {
-    local src="$1" dest="$2"
-    [[ ! -d "$src" ]] && { warn "Source not found: $src"; return; }
-    run "mkdir -p \"$dest\"" "Creating $dest"
-    run "cp -r \"$src\"/* \"$dest\"" "Copy $src -> $dest"
-    run "chown -R $USER_NAME:$USER_NAME \"$dest\"" "Fix ownership of $dest"
-}
+# Check root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit 1
+fi
 
-# --- Update system ---
-run "pacman -Syyu --noconfirm" "Updating system packages"
+print_header "Updating system packages"
+run_command "pacman -Syyu --noconfirm"
 
-# --- Install pacman packages ---
-PACMAN_PACKAGES=(
-    pipewire wireplumber pamixer brightnessctl
-    ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans
-    ttf-iosevka-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
-    sddm kitty nano tar gnome-disk-utility code mpv dunst pacman-contrib exo
-    thunar thunar-archive-plugin thunar-volman tumbler ffmpegthumbnailer file-roller
-    gvfs gvfs-mtp gvfs-gphoto2 gvfs-smb
-    polkit polkit-gnome waybar firefox
-)
-run "pacman -S --noconfirm ${PACMAN_PACKAGES[*]}" "Installing system packages"
-
-run "systemctl enable --now polkit.service" "Enable polkit"
-run "systemctl enable sddm.service" "Enable SDDM"
-
-# --- Install yay if missing ---
+# Install yay if missing
 if ! command -v yay &>/dev/null; then
-    log "Installing yay..."
-    run "pacman -S --noconfirm --needed git base-devel" "Install git/base-devel"
-    run "git clone https://aur.archlinux.org/yay.git /tmp/yay" "Clone yay"
-    run "chown -R $USER_NAME:$USER_NAME /tmp/yay" "Fix ownership"
-    run "sudo -u $USER_NAME bash -c 'cd /tmp/yay && makepkg -si --noconfirm'" "Build and install yay"
-    run "rm -rf /tmp/yay" "Clean yay build"
+  print_info "Installing yay..."
+  pacman -S --noconfirm --needed git base-devel
+  git clone https://aur.archlinux.org/yay.git /tmp/yay
+  chown -R $SUDO_USER:$SUDO_USER /tmp/yay
+  sudo -u $SUDO_USER bash -c "cd /tmp/yay && makepkg -si --noconfirm"
+  rm -rf /tmp/yay
+else
+  print_success "Yay is already installed"
 fi
 
-# --- Install AUR packages as user ---
-AUR_PACKAGES=(wofi fastfetch swww hyprpicker hyprlock grimblast hypridle starship)
-for pkg in "${AUR_PACKAGES[@]}"; do
-    log "Installing AUR package: $pkg"
-    sudo -u $USER_NAME yay -S --sudoloop --noconfirm "$pkg"
+# System packages
+PACKAGES=(
+  pipewire wireplumber pamixer brightnessctl
+  ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans
+  ttf-iosevka-nerd ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono
+  sddm kitty nano tar gnome-disk-utility code mpv dunst pacman-contrib exo
+  thunar thunar-archive-plugin thunar-volman tumbler ffmpegthumbnailer file-roller
+  gvfs gvfs-mtp gvfs-gphoto2 gvfs-smb polkit polkit-gnome
+)
+run_command "pacman -S --noconfirm ${PACKAGES[*]}"
+run_command "systemctl enable --now polkit.service"
+run_command "systemctl enable sddm.service"
+
+# Yay apps
+YAY_APPS=(
+  firefox wofi fastfetch swww hyprpicker hyprlock grimblast hypridle starship
+)
+for app in "${YAY_APPS[@]}"; do
+  sudo -u $SUDO_USER yay -S --noconfirm "$app"
 done
 
-# --- Copy config assets ---
-copy_as_user "$ASSETS_SRC/backgrounds" "$ASSETS_DEST/backgrounds"
-
-# --- Waybar config ---
-copy_as_user "$REPO_DIR/configs/waybar" "$CONFIG_DIR/waybar"
-
-# --- Wofi config ---
-copy_as_user "$REPO_DIR/configs/wofi" "$CONFIG_DIR/wofi"
-
-# --- Fastfetch config ---
-copy_as_user "$REPO_DIR/configs/fastfetch" "$CONFIG_DIR/fastfetch"
-
-# --- Hypr config ---
-copy_as_user "$REPO_DIR/configs/hypr" "$CONFIG_DIR/hypr"
-
-# --- Starship config ---
-STARSHIP_SRC="$REPO_DIR/configs/starship/starship.toml"
-STARSHIP_DEST="$CONFIG_DIR/starship.toml"
-[[ -f "$STARSHIP_SRC" ]] && cp "$STARSHIP_SRC" "$STARSHIP_DEST" && chown "$USER_NAME:$USER_NAME" "$STARSHIP_DEST"
-
-# --- Add Fastfetch to shell ---
-for shell_rc in ".bashrc" ".zshrc"; do
-    line="fastfetch --kitty-direct $CONFIG_DIR/fastfetch/archkitty.png"
-    path="$USER_HOME/$shell_rc"
-    [[ -f "$path" ]] && ! grep -qF "$line" "$path" && echo -e "\n# Run fastfetch on terminal start\n$line" >> "$path" && chown "$USER_NAME:$USER_NAME" "$path"
-done
-
-# --- Add Starship to shell ---
-for shell_rc in ".bashrc" ".zshrc"; do
-    shell_name="${shell_rc#*.}"
-    line='eval "$(starship init '"$shell_name"')"'
-    path="$USER_HOME/$shell_rc"
-    [[ -f "$path" ]] && ! grep -qF "$line" "$path" && echo -e "\n$line" >> "$path" && chown "$USER_NAME:$USER_NAME" "$path"
-done
-
-# --- Oh My Zsh ---
+# Oh-My-Zsh
 if [ ! -d "$USER_HOME/.oh-my-zsh" ]; then
-    log "Installing Oh My Zsh"
-    sudo -u $USER_NAME sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    sudo -u $USER_NAME chsh -s "$(which zsh)"
+  sudo -u $SUDO_USER sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  sudo -u $SUDO_USER chsh -s "$(which zsh)" $SUDO_USER
 fi
 
-# --- Dracula SDDM theme ---
-DRACULA_SDDM_REPO="https://github.com/dracula/sddm.git"
-DRACULA_TEMP="/tmp/dracula-sddm"
-git clone --depth=1 "$DRACULA_SDDM_REPO" "$DRACULA_TEMP"
-cp -r "$DRACULA_TEMP/sddm/themes/dracula" "/usr/share/sddm/themes/dracula"
-chown -R root:root "/usr/share/sddm/themes/dracula"
+# Dracula SDDM Theme
+print_header "Installing Dracula SDDM Theme"
+git clone https://github.com/nopain2110/Yet-another-dracula.git /tmp/dracula-theme
+sudo cp -r /tmp/dracula-theme/sddm-dracula /usr/share/sddm/themes/dracula
+sudo rm -rf /tmp/dracula-theme
 mkdir -p /etc/sddm.conf.d
-echo -e "[Theme]\nCurrent=dracula" > /etc/sddm.conf.d/10-theme.conf
-rm -rf "$DRACULA_TEMP"
+echo "CurrentTheme=dracula" | sudo tee /etc/sddm.conf.d/dracula.conf
+print_success "Dracula SDDM theme installed"
 
-# --- Dracula icons ---
-DRACULA_ICONS_REPO="https://github.com/dracula/gtk-theme.git"
-ICONS_TEMP="/tmp/dracula-icons"
-git clone --depth=1 "$DRACULA_ICONS_REPO" "$ICONS_TEMP"
-copy_as_user "$ICONS_TEMP/icons" "$CONFIG_DIR/icons"
-rm -rf "$ICONS_TEMP"
+# Dracula GTK and Icon Themes
+print_header "Installing Dracula GTK and Icon Themes"
+# Icons
+sudo -u $SUDO_USER mkdir -p $USER_HOME/.icons
+git clone https://github.com/dracula/gtk.git /tmp/dracula-gtk
+sudo -u $SUDO_USER cp -r /tmp/dracula-gtk/icons/* $USER_HOME/.icons/
+# GTK themes
+sudo -u $SUDO_USER mkdir -p $USER_HOME/.themes
+sudo -u $SUDO_USER cp -r /tmp/dracula-gtk/gtk/* $USER_HOME/.themes/
+sudo rm -rf /tmp/dracula-gtk
 
-# --- GTK settings ---
-GTK3_DIR="$CONFIG_DIR/gtk-3.0"
-GTK4_DIR="$CONFIG_DIR/gtk-4.0"
-mkdir -p "$GTK3_DIR" "$GTK4_DIR"
-GTK_SETTINGS="[Settings]
-gtk-theme-name=Dracula
-gtk-icon-theme-name=Dracula
-gtk-font-name=JetBrainsMono 10"
-echo "$GTK_SETTINGS" | sudo -u "$USER_NAME" tee "$GTK3_DIR/settings.ini" "$GTK4_DIR/settings.ini" >/dev/null
+# Set GTK theme and icon theme for user
+GTK_SETTINGS="$USER_HOME/.config/gtk-3.0/settings.ini"
+mkdir -p "$(dirname "$GTK_SETTINGS")"
+cat << EOF > "$GTK_SETTINGS"
+[Settings]
+gtk-theme-name = Dracula
+gtk-icon-theme-name = Dracula
+gtk-font-name = Fira Code 10
+EOF
+chown $SUDO_USER:$SUDO_USER "$GTK_SETTINGS"
+print_success "Dracula GTK and Icon themes installed"
 
-log "Setup completed! Reboot recommended."
+print_success "Setup completed!"
