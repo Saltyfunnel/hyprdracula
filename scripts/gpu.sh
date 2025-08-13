6c0f6a9008284be79f8cb087d8e4ff202fbfd2aa
Saltyfunnel <gerardsheal@gmail.com>
2025-08-13 14:53:06 +0100

    Update gpu.sh

--- scripts/gpu.sh
+++ scripts/gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 source "$SCRIPT_DIR/helper.sh"
 
 print_info "\nDetecting GPU..."
