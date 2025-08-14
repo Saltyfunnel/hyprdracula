598c29f82653e1bb3dd08632ee66e00607dc7ef9
Saltyfunnel <gerardsheal@gmail.com>
2025-08-14 12:04:05 +0100

    Update utilities.sh

--- scripts/utilities.sh
+++ scripts/utilities.sh
@@ -24,28 +24,64 @@ copy_as_user() {
         return 1
     fi
 
-    mkdir -p "$dest"
-    cp -r "$src"/* "$dest"
-    chown -R "$USER_NAME:$USER_NAME" "$dest"
+    run_command "mkdir -p \"$dest\"" "Create destination directory $dest" "no" "no"
+    run_command "cp -r \"$src\"/* \"$dest\"" "Copy from $src to $dest" "yes" "no"
+    run_command "chown -R $USER_NAME:$USER_NAME \"$dest\"" "Fix ownership for $dest" "no" "yes"
 }
 
-# --- Install core utilities ---
-pacman -S --noconfirm waybar
-copy_as_user "$REPO_DIR/configs/waybar" "$CONFIG_DIR/waybar"
+# ----------------------------
+# Core Utilities
+# ----------------------------
+run_command "pacman -S --noconfirm waybar" "Install Waybar" "yes"
+[ -d "$REPO_DIR/configs/waybar" ] && copy_as_user "$REPO_DIR/configs/waybar" "$CONFIG_DIR/waybar"
 
-yay -S --noconfirm tofi fastfetch swww hyprpicker hyprlock grimblast hypridle starship spotify protonplus
+# ----------------------------
+# AUR Utilities via yay (as user)
+# ----------------------------
+AUR_PACKAGES="tofi fastfetch swww hyprpicker hyprlock grimblast hypridle starship spotify protonplus"
+sudo -u "$USER_NAME" bash -c "yay -S --noconfirm $AUR_PACKAGES"
 
-copy_as_user "$REPO_DIR/configs/tofi" "$CONFIG_DIR/tofi"
-copy_as_user "$REPO_DIR/configs/fastfetch" "$CONFIG_DIR/fastfetch"
-copy_as_user "$REPO_DIR/configs/hypr" "$CONFIG_DIR/hypr"
-copy_as_user "$REPO_DIR/configs/kitty" "$CONFIG_DIR/kitty"
-copy_as_user "$REPO_DIR/configs/dunst" "$CONFIG_DIR/dunst"
+for dir in tofi fastfetch hypr kitty dunst; do
+    [ -d "$REPO_DIR/configs/$dir" ] && copy_as_user "$REPO_DIR/configs/$dir" "$CONFIG_DIR/$dir"
+done
 
-# --- Add fastfetch to shell ---
+# ----------------------------
+# Dracula GTK Theme
+# ----------------------------
+THEMES_DIR="$USER_HOME/.themes"
+sudo -u "$USER_NAME" mkdir -p "$THEMES_DIR"
+
+# Copy Dracula GTK theme if it exists in repo
+[ -d "$REPO_DIR/assets/themes/Dracula" ] && copy_as_user "$REPO_DIR/assets/themes/Dracula" "$THEMES_DIR/Dracula"
+
+GTK3_CONFIG_DIR="$CONFIG_DIR/gtk-3.0"
+GTK4_CONFIG_DIR="$CONFIG_DIR/gtk-4.0"
+sudo -u "$USER_NAME" mkdir -p "$GTK3_CONFIG_DIR" "$GTK4_CONFIG_DIR"
+
+GTK_SETTINGS_CONTENT="[Settings]
+gtk-theme-name=Dracula
+gtk-icon-theme-name=Papirus-Dark
+gtk-font-name=JetBrainsMono 10"
+
+echo "$GTK_SETTINGS_CONTENT" | sudo -u "$USER_NAME" tee "$GTK3_CONFIG_DIR/settings.ini" "$GTK4_CONFIG_DIR/settings.ini" >/dev/null
+chown -R "$USER_NAME:$USER_NAME" "$GTK3_CONFIG_DIR" "$GTK4_CONFIG_DIR"
+
+# Apply theme via gsettings as user
+sudo -u "$USER_NAME" dbus-launch gsettings set org.gnome.desktop.interface gtk-theme 'Dracula'
+sudo -u "$USER_NAME" dbus-launch gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+
+# ----------------------------
+# Assets
+# ----------------------------
+[ -d "$ASSETS_SRC/backgrounds" ] && copy_as_user "$ASSETS_SRC/backgrounds" "$ASSETS_DEST/backgrounds"
+
+# ----------------------------
+# Fastfetch in shell
+# ----------------------------
 add_fastfetch_to_shell() {
     local shell_rc="$1"
     local shell_rc_path="$USER_HOME/$shell_rc"
-    local fastfetch_line="fastfetch --kitty-direct $CONFIG_DIR/fastfetch/archkitty.png"
+    local fastfetch_line='fastfetch --kitty-direct /home/'"$USER_NAME"'/.config/fastfetch/archkitty.png'
 
     if [ -f "$shell_rc_path" ] && ! grep -qF "$fastfetch_line" "$shell_rc_path"; then
         echo -e "\n# Run fastfetch on terminal start\n$fastfetch_line" >> "$shell_rc_path"
@@ -54,22 +90,20 @@ add_fastfetch_to_shell() {
 }
 
 add_fastfetch_to_shell ".bashrc"
-add_fastfetch_to_shell ".zshrc"
 
-# --- Starship config ---
+# ----------------------------
+# Starship
+# ----------------------------
 STARSHIP_SRC="$REPO_DIR/configs/starship/starship.toml"
 STARSHIP_DEST="$CONFIG_DIR/starship.toml"
 
-if [ -f "$STARSHIP_SRC" ]; then
-    cp "$STARSHIP_SRC" "$STARSHIP_DEST"
-    chown "$USER_NAME:$USER_NAME" "$STARSHIP_DEST"
-fi
+[ -f "$STARSHIP_SRC" ] && cp "$STARSHIP_SRC" "$STARSHIP_DEST" && chown "$USER_NAME:$USER_NAME" "$STARSHIP_DEST"
 
 add_starship_to_shell() {
     local shell_rc="$1"
     local shell_name="$2"
     local shell_rc_path="$USER_HOME/$shell_rc"
-    local starship_line="eval \"\$(starship init $shell_name)\""
+    local starship_line='eval "$(starship init '"$shell_name"')"' 
 
     if [ -f "$shell_rc_path" ] && ! grep -qF "$starship_line" "$shell_rc_path"; then
         echo -e "\n$starship_line" >> "$shell_rc_path"
@@ -78,82 +112,5 @@ add_starship_to_shell() {
 }
 
 add_starship_to_shell ".bashrc" "bash"
-add_starship_to_shell ".zshrc" "zsh"
-
-# --- Install additional utilities ---
-pacman -S --noconfirm cliphist
-copy_as_user "$ASSETS_SRC/backgrounds" "$ASSETS_DEST/backgrounds"
-
-# --- Dracula GTK Theme ---
-GTK3_CONFIG_DIR="$CONFIG_DIR/gtk-3.0"
-GTK4_CONFIG_DIR="$CONFIG_DIR/gtk-4.0"
-THEME_REPO="$USER_HOME/Dracula-gtk-theme"
-
-if [ ! -d "$THEME_REPO" ]; then
-    git clone https://github.com/dracula/gtk.git "$THEME_REPO"
-fi
-
-mkdir -p "$USER_HOME/.themes"
-cp -r "$THEME_REPO"/* "$USER_HOME/.themes/"
-chown -R "$USER_NAME:$USER_NAME" "$USER_HOME/.themes"
-
-# Apply Dracula GTK theme
-mkdir -p "$GTK3_CONFIG_DIR" "$GTK4_CONFIG_DIR"
-
-cat > "$GTK3_CONFIG_DIR/settings.ini" << EOF
-[Settings]
-gtk-theme-name=Dracula
-gtk-icon-theme-name=Papirus-Dark
-gtk-font-name=JetBrains Mono 11
-EOF
-
-cat > "$GTK4_CONFIG_DIR/settings.ini" << EOF
-[Settings]
-gtk-theme-name=Dracula
-gtk-icon-theme-name=Papirus-Dark
-gtk-font-name=JetBrains Mono 11
-EOF
-
-chown -R "$USER_NAME:$USER_NAME" "$GTK3_CONFIG_DIR" "$GTK4_CONFIG_DIR"
-
-# --- Thunar Kitty custom action ---
-setup_thunar_kitty_action() {
-    local uca_dir="$CONFIG_DIR/Thunar"
-    local uca_file="$uca_dir/uca.xml"
-
-    mkdir -p "$uca_dir"
-    chown "$USER_NAME:$USER_NAME" "$uca_dir"
-    chmod 700 "$uca_dir"
-
-    local kitty_action_xml='
-  <action>
-    <icon>utilities-terminal</icon>
-    <name>Open Kitty Here</name>
-    <command>kitty --directory=%d</command>
-    <description>Open kitty terminal in the current folder</description>
-    <patterns>*</patterns>
-    <directories_only>true</directories_only>
-    <startup_notify>true</startup_notify>
-  </action>'
-
-    if [ ! -f "$uca_file" ]; then
-        cat > "$uca_file" << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<actions>
-$kitty_action_xml
-</actions>
-EOF
-        chown "$USER_NAME:$USER_NAME" "$uca_file"
-    else
-        if ! grep -q "<name>Open Kitty Here</name>" "$uca_file"; then
-            sed -i "/<\/actions>/ i\\
-$kitty_action_xml
-" "$uca_file"
-            chown "$USER_NAME:$USER_NAME" "$uca_file"
-        fi
-    fi
-}
-
-setup_thunar_kitty_action
 
 print_success "\nUtilities setup complete!"
