3f2f50159787bc2077fe3eaccc2e15fcc4e3b037
Saltyfunnel <gerardsheal@gmail.com>
2025-08-19 16:17:25 +0100

    Update sddm.sh

--- scripts/sddm.sh
+++ scripts/sddm.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-
-# Script to install Dracula-inspired SDDM theme on Arch Linux
-
 set -e
 
-# Variables
 THEME_NAME="Yet-another-dracula"
 THEME_REPO="https://github.com/trancong12102/Yet-another-dracula.git"
 SDDM_DIR="/usr/share/sddm/themes"
 
-echo "==> Installing required packages..."
+# Install dependencies
 sudo pacman -S --noconfirm sddm qt5-declarative git
 
-echo "==> Cloning the Dracula SDDM theme..."
+# Clone the theme
 git clone "$THEME_REPO" /tmp/"$THEME_NAME"
 
-echo "==> Copying theme to SDDM directory..."
-sudo cp -r /tmp/"$THEME_NAME"/SDDM "$SDDM_DIR/$THEME_NAME"
-
-echo "==> Setting SDDM theme..."
-if ! grep -q "Current=" /etc/sddm.conf 2>/dev/null; then
-    # If the config doesn't exist, create it
-    echo -e "[Theme]\nCurrent=$THEME_NAME" | sudo tee /etc/sddm.conf
+# Detect correct theme folder
+if [ -d "/tmp/$THEME_NAME/SDDM" ]; then
+    THEME_SRC="/tmp/$THEME_NAME/SDDM"
 else
-    sudo sed -i "s/^Current=.*/Current=$THEME_NAME/" /etc/sddm.conf
+    THEME_SRC="/tmp/$THEME_NAME"
 fi
 
-echo "==> Cleaning up..."
-rm -rf /tmp/"$THEME_NAME"
+# Copy theme
+sudo cp -r "$THEME_SRC" "$SDDM_DIR/$THEME_NAME"
 
-echo "==> Restarting SDDM to apply theme..."
-sudo systemctl restart sddm
+# Set permissions
+sudo chown -R root:root "$SDDM_DIR/$THEME_NAME"
 
-echo "==> Done! Your SDDM theme should now be set to $THEME_NAME."
+# Create or update config
+if [ ! -f /etc/sddm.conf ]; then
+    echo -e "[Theme]\nCurrent=$THEME_NAME" | sudo tee /etc/sddm.conf
+else
+    sudo sed -i '/^\[Theme\]/,/^\[/ s/^Current=.*/Current='"$THEME_NAME"'/' /etc/sddm.conf || \
+    echo -e "[Theme]\nCurrent=$THEME_NAME" | sudo tee -a /etc/sddm.conf
+fi
+
+echo "==> Done. Please reboot or switch to a TTY and run 'sudo systemctl restart sddm' to see the theme."
